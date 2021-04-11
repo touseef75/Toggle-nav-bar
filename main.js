a57fdca49f48a33e1e7f83b65bca1b7d087cfc81
touseef75 <imrantouseef776@gmail.com>
2021-04-11 04:22:30 -0700

    complete responive navbar

---
+++ main.js
@@ -0,0 +1,99 @@
+let but = document.querySelector("span")
+
+but.addEventListener("click",()=>{
+let nav = document.querySelector("nav")
+    if(nav.style.display == "none"){
+        nav.style.display = "block";
+        nav.style.width = "16rem"
+        // nav.style.transition = "all .5s"
+    }else{
+        nav.style.display = "none";
+
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let main = document.getElementById("cont")
+// // let conti = document.getElementsByClassName("container")
+// let cli = document.querySelector("button")
+
